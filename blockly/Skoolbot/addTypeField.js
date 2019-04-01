7b6e043c1c42848f652e68ae48e9a17865c94023
lingyuan <lingyuan.au@gmail.com>
2019-04-01 16:07:30 +1100

    complete the type field

--- blockly/Skoolbot/addTypeField.js
+++ blockly/Skoolbot/addTypeField.js
@@ -5,41 +5,61 @@ function addTypeField(jsonlist){
             addTypeField(jsonlist[element]);
         }
         else{
-                        var hasChild = false;
-            for (var k in jsonlist){
-                if (jsonlist[k] instanceof Array){
-                    hasChild = true;
-                }
-            }
-            if(!hasChild) {
-                var blockName = jsonlist.block_name;
-                console.log('block_name: ', blockName);
-                if(blockName!=undefined){
-                    var block_type = blockName.split('_')[0];
-                    console.log('block_type: ', block_type);
-                    switch (block_type) {
-                        case 'math':
-                            jsonlist.valueType = "number";
-                            break;
-                        case 'colour':
-                            jsonlist.valueType = "colour";
-                            break;
-                        default:
-                            jsonlist.valueType = "null";
-                            break;
+            var blockName = jsonlist.block_name;
+            // console.log('block_name: ', blockName);
+            if(blockName!=undefined){
+                var block_type = blockName.split('_')[1];
+                // console.log('block_type: ', block_type);
 
-                    }
+                //add value type to bottom level of block in JSON
+                switch (block_type) {
+                    case 'arithmetic':
+                        console.log(jsonlist.argument);
+                        var type_list = [];
+                        for (var j in jsonlist.argument){
+                            type_list.push(jsonlist.argument[j].valueType);
+                        }
+                        console.log(type_list);
+
+                        jsonlist.valueType = checkTypeList(type_list);
+                        break;
+                    default:
+                        jsonlist.valueType = block_type;
+                        break;
                 }
             }
-            else{
-                //TODO
-            }
+
+            //TODO
+            // add type field based on the operation type and its direct children's value type.
         }
     }
 }
 
-var str = JSON.parse(`[{"block_name":"procedures_defreturn","procedures_type":"procedures_defreturn","funcName":"print_colour","description":"--Describethisfunction...","argument":[],"branch":[{"block_name":"variables_set","functionName":"variables_set","varName":"colour","argument":[{"block_name":"colour_picker","colour":"#ff0000","valueType":""}],"valueType":""},{"block_name":"text_print","functionName":"text_print","argument":[{"block_name":"variables_get","functionName":"variables_get","varName":"colour","valueType":""}],"valueType":""}],"return_value":{"null":"NULL","valueType":""},"valueType":""},{"block_name":"colour_picker","colour":"#ff0000","valueType":""}]`);
+// function hasChild(jsonlist) {
+//     var hasChild = false;
+//     for (var k in jsonlist){
+//         if (jsonlist[k] instanceof Array){
+//             hasChild = true;
+//         }
+//     }
+// }
 
-console.log('jsonlist: \n', str);
+
+function checkTypeList(typeList){
+    var type = typeList[0];
+    for (var i in typeList){
+        if(typeList[i] != type){
+            return '';
+        }
+    }
+    return type;
+}
+
+
+
+var str = JSON.parse(`[{"block_name":"controls_statement_repeat","loop_style":"controls_repeat_ext","repeat_times":{"block_name":"controls_statement_repeatExt","operator":"math.floor","argument":{"block_name":"math_number_number","number":"10","valueType":""},"valueType":""},"branch":[{"block_name":"controls_statement_ifStructure","structure":[{"block_name":"controls_statement_if","statements":"if","condition":{"block_name":"logic_boolean_compare","operator":">","argument":[{"block_name":"text_number_count","functionName":"text_count","argument":[{"block_name":"variables_statement_get","functionName":"variables_get","varName":"i","valueType":""},{"block_name":"text_string_substring","substring":{"block_name":"text_string","text":"'Hi'","valueType":""},"valueType":""}],"valueType":""},{"block_name":"math_number_number","number":"0","valueType":""}],"valueType":""},"branchCode":[{"block_name":"variables_statement_set","functionName":"variables_set","varName":"i","argument":[{"block_name":"text_string_getSubstring","functionName":"text_getSubstring","argument":[{"block_name":"variables_variable_variable","varName":{"block_name":"variables_statement_get","functionName":"variables_get","varName":"text","valueType":""},"valueType":""},{"block_name":"text_string_startWhere","start_where":"FROM_START","valueType":""},{"block_name":"text_number_startAt","start_index":{"block_name":"math_number_number","number":"12","valueType":""},"valueType":""},{"block_name":"text_string_endWhere","end_where":"FROM_END","valueType":""},{"block_name":"text_number_endAt","end_index":{"block_name":"math_number_number","number":"3","valueType":""},"valueType":""}],"valueType":""}],"valueType":""}],"valueType":""},{"block_name":"controls_statement_else","statements":"else","branchCode":{"block_name":"text_statement_print","functionName":"text_print","argument":[{"block_name":"math_arithmetic_arithmetic","operator":"+","argument":[{"block_name":"math_number_number","number":"1","valueType":""},{"block_name":"math_number_number","number":"1","valueType":""}],"valueType":""}],"valueType":""},"valueType":""}],"valueType":""}],"valueType":""}]
+`);
+
+console.log('jsonlist: \n', JSON.stringify(str));
 addTypeField(str);
 console.log("addTypeField: \n", JSON.stringify(str));
--- blockly/Skoolbot/treeWalkJSON.js
+++ blockly/Skoolbot/treeWalkJSON.js
@@ -7,7 +7,7 @@ function printJsonTree(jsonlist, key) {
     for (element in jsonlist) {
         var k = key + ' -> ' + element;
         if ((jsonlist[element] instanceof Object)) {
-            jsonlist[element].valueType = "\"\"";
+            jsonlist[element].valueType = "";
             printJsonTree(jsonlist[element], k);
         }
         else {
@@ -19,13 +19,11 @@ function printJsonTree(jsonlist, key) {
 }
 
 
-function hasChildJSON(){
-    //TODO
-    return false;
-}
 
 
-var str = JSON.parse(`[{"block_name":"procedures_defreturn","procedures_type":"procedures_defreturn","funcName":"print_colour","description":"--Describethisfunction...","argument":[],"branch":[{"block_name":"variables_set","functionName":"variables_set","varName":"colour","argument":[{"block_name":"colour_picker","colour":"#ff0000"}]},{"block_name":"text_print","functionName":"text_print","argument":[{"block_name":"variables_get","functionName":"variables_get","varName":"colour"}]}],"return_value":{"null":"NULL"}}]`);
+
+var str = JSON.parse(`[{"block_name":"controls_statement_repeat","loop_style":"controls_repeat_ext","repeat_times":{"block_name":"controls_statement_repeatExt","operator":"math.floor","argument":{"block_name":"math_number_number","number":"10"}},"branch":[{"block_name":"controls_statement_ifStructure","structure":[{"block_name":"controls_statement_if","statements":"if","condition":{"block_name":"logic_boolean_compare","operator":">","argument":[{"block_name":"text_number_count","functionName":"text_count","argument":[{"block_name":"variables_statement_get","functionName":"variables_get","varName":"i"},{"block_name":"text_string_substring","substring":{"block_name":"text_string","text":"'Hi'"}}]},{"block_name":"math_number_number","number":"0"}]},"branchCode":[{"block_name":"variables_statement_set","functionName":"variables_set","varName":"i","argument":[{"block_name":"text_string_getSubstring","functionName":"text_getSubstring","argument":[{"block_name":"variables_variable_variable","varName":{"block_name":"variables_statement_get","functionName":"variables_get","varName":"text"}},{"block_name":"text_string_startWhere","start_where":"FROM_START"},{"block_name":"text_number_startAt","start_index":{"block_name":"math_number_number","number":"12"}},{"block_name":"text_string_endWhere","end_where":"FROM_END"},{"block_name":"text_number_endAt","end_index":{"block_name":"math_number_number","number":"3"}}]}]}]},{"block_name":"controls_statement_else","statements":"else","branchCode":{"block_name":"text_statement_print","functionName":"text_print","argument":[{"block_name":"math_arithmetic_arithmetic","operator":"+","argument":[{"block_name":"math_number_number","number":"1"},{"block_name":"math_number_number","number":"1"}]}]}}]}]}]
+`);
 
 console.log("jsonlist: \n", JSON.stringify(str));
 
