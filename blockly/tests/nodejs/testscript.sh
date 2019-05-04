8109377e83fe13b7b22f52a72d67de7a2c54b4cd
lingyuan <lingyuan.au@gmail.com>
2019-05-04 14:28:20 +1000

    restructure the generator functions

--- blockly/tests/nodejs/testscript.sh
+++ blockly/tests/nodejs/testscript.sh
@@ -1,147 +1,4 @@
-<<<<<<< HEAD
 #!/bin/bash
-
-## compile the interpreter
-#g++ -o ../../SkoolBot/interpreter ../../SkoolBot/interpreter.cpp
-#
-#for xml in test_cases/*.xml
-#do
-#    # conver xml to json
-#    json="test_jsons/`basename $xml .xml`.json"
-#
-#    if [ -e $json ]
-#      then
-#        node ../../SkoolBot/xmlToJson.js $xml > compare.json
-#        result=$(wdiff -3 $json compare.json)
-#        if [ $? -eq 0 ]
-#           then
-#             echo -e "$json Test case pass."
-#           else
-#             echo "$result"
-#             echo -e "$json Test case failed."
-#             break
-#        fi
-#    else
-#        node ../../SkoolBot/xmlToJson.js $xml > $json
-#        echo "created $json."
-#    fi
-#
-#   #  # add type field to json
-#   #  addTypeField="test_typeField/`basename $xml .xml`.json"
-#   #  if [ -e $addTypeField ]
-#   #  then
-#   #      node main.js $xml true > compare.json
-#   #      result=$(wdiff -3 $addTypeField compare.json)
-#   #      if [ $? -eq 0 ]
-#   #         then
-#   #           echo -e "$addTypeField Test case pass."
-#   #         else
-#   #           echo "$result"
-#   #           echo -e "$addTypeField Test case failed."
-#   #           break
-#   #      fi
-#   # else
-#   #    node main.js $xml true > $addTypeField
-#   #    echo "created $addTypeField."
-#   # fi
-#
-#   # byte code generator
-#   # generator="generator_test_jsons/`basename $xml .xml`.json"
-#   # if [ -e $generator ]
-#   #   then
-#   #     ### execute javascript with $x > $$.out
-#   #     node main.js $x> compare.json
-#   #     result=$(wdiff -3 $generator compare.json)
-#   #     if [ $? -eq 0 ]
-#   #       then
-#   #          echo -e "$generator Test case pass.\n"
-#   #        else
-#   #          echo "$result"
-#   #          echo -e "$generator Test case failed.\n"
-#   #          break
-#   #     fi
-#   # else
-#   #   node main.js $x > $generator
-#   #   echo "created $generator"
-#   # fi
-#   # rm compare.json
-#   # echo -e ""
-#   #
-#   # # interpreter
-#   # interpreter="interpreter_outputs/`basename $x .txt`.txt"
-#   # if [ -e $interpreter ]
-#   #   then
-#   #     ### execute javascript with $x > $$.out
-#   #     ../../SkoolBot/interpreter $x > compare.txt
-#   #     result=$(wdiff -3 $interpreter compare.txt)
-#   #     if [ $? -eq 0 ]
-#   #        then
-#   #          echo -e "$interpreter Test case pass."
-#   #        else
-#   #          echo "$result"
-#   #          echo -e "$interpreter Test case failed."
-#   #          break
-#   #     fi
-#   # else
-#   #     ../../SkoolBot/interpreter $x > $interpreter
-#   #     echo "created $interpreter."
-#   # fi
-#   # rm compare.txt
-#   # echo -e ""
-#   # echo -e ""
-#   # echo -e ""
-#done
-
-# # bytecode generater
-#
-# #!/bin/bash
-# for x in generator_test_cases/*.xml
-# do
-#     generator="generator_test_jsons/`basename $x .xml`.json"
-#     if [ -e $generator ]
-#       then
-#         ### execute javascript with $x > $$.out
-#         node main.js $x true> compare.json
-#         result=$(wdiff -3 $generator compare.json)
-#         if [ $? -eq 0 ]
-#           then
-#              echo -e "$generator Test case pass.\n"
-#            else
-#              echo "$result"
-#              echo -e "$generator Test case failed.\n"
-#         fi
-#     else
-#       node main.js $x > $generator
-#       echo "created $generator"
-#     fi
-# done
-#
-#
- # for testing the interpreter
- # compile
- g++ -o ../../SkoolBot/interpreter ../../SkoolBot/interpreter.cpp
- for x in generator_outputs/*.txt
- do
-     interpreter="interpreter_outputs/`basename $x .txt`.txt"
-     if [ -e $interpreter ]
-       then
-         ### execute javascript with $x > $$.out
-         ../../SkoolBot/interpreter $x > compare.txt
-         result=$(wdiff -3 $interpreter compare.txt)
-         if [ $? -eq 0 ]
-            then
-              echo -e "$interpreter Test case pass."
-            else
-              echo "$result"
-              echo -e "$interpreter Test case failed."
-         fi
-     else
-         ../../SkoolBot/interpreter $x > $interpreter
-         echo "created $interpreter."
-     fi
- done
-=======
-# !/bin/bash
 
 # compile the interpreter
 g++ -o ../../SkoolBot/interpreter ../../SkoolBot/interpreter.cpp
@@ -189,12 +46,12 @@ do
       echo "created $addTypeField."
    fi
 
-   # bytecode generator
+   # command generator
    generator="generatorToInterpreter_test_cases/`basename $xml .xml`.txt"
    if [ -e $generator ]
      then
        ### execute javascript with $x > $$.out
-       node ../../SkoolBot/bytecode_generator.js $addTypeField > compare.json
+       node ../../SkoolBot/command_generator.js $addTypeField > compare.json
        result=$(wdiff -3 $generator compare.json)
        if [ $? -eq 0 ]
          then
@@ -207,7 +64,7 @@ do
        fi
        rm compare.json
    else
-     node ../../SkoolBot/bytecode_generator.js $addTypeField > $generator
+     node ../../SkoolBot/command_generator.js $addTypeField > $generator
      echo "created $generator."
    fi
 
@@ -238,8 +95,8 @@ do
    echo ""
 done
 
-#! for testing the interpreter
-# compile
+# # for testing the interpreter
+# # compile
 # g++ -o ../../SkoolBot/interpreter ../../SkoolBot/interpreter.cpp
 # for x in generatorToInterpreter_test_cases/*.txt
 # do
@@ -261,4 +118,4 @@ done
 #         echo "created $interpreter."
 #     fi
 # done
->>>>>>> json
+
