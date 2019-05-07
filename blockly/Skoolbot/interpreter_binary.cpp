c02c5ea34e304397c24acd7f7b4cd5ccece90128
JayUni <jzhe3972@uni.sydney.edu.au>
2019-05-08 05:14:19 +1000

    finished interpreter binary but not tested

--- blockly/Skoolbot/interpreter_binary.cpp
+++ blockly/Skoolbot/interpreter_binary.cpp
@@ -1,345 +1,323 @@
-#define NOP_ 0x00
-#define NUMBER 0x01
-#define ADD 0x02
-#define SUB 0x03
-#define MUL 0x04
-#define DIV 0x05
-#define POW 0x06
-#define ABS 0x07
-#define NEG 0x08
-#define ISEVEN 0x09
-#define ISODD 0x0A
-#define ISPOSITIVE 0x0B
-#define ISNEGATIVE 0x0C
-#define ISDIVISBLEBY 0x0D
-#define REMAINDER 0x0E
-#define CONSTRAIN 0x0F
-#define RANDOMINT 0x10
-#define CMPE 0x11
-#define CMPNE 0x12
-#define CMPL 0x13
-#define CMPLE 0x14
-#define CMPG 0x15
-#define CMPGE 0x16
-#define TRUE  0x17
-#define FALSE 0x18
-#define NEGATE 0x19
-#define NULL_ 0x1A
-#define GET 0x1B
-#define SET 0x1C
-#define JUMPZ 0x1D
-#define JUMP 0x1E
-#define PRINT 0x1F
-#define BOOLEAN 0x20
-
 #include <iostream>
-#include <fstream>
 #include <stack>
 #include <string>
-#include <vector>
 #include <map>
 #include <cmath>
+#include <assert.h>
 
-// #include <stdint.h>
+#define NOP_          (0x00)
+#define NUMBER        (0x01)
+#define ADD           (0x02)
+#define SUB           (0x03)
+#define MUL           (0x04)
+#define DIV           (0x05)
+#define POW           (0x06)
+#define ABS           (0x07)
+#define NEG           (0x08)
+#define ISEVEN        (0x09)
+#define ISODD         (0x0A)
+#define ISPOSITIVE    (0x0B)
+#define ISNEGATIVE    (0x0C)
+#define ISDIVISBLEBY  (0x0D)
+#define REMAINDER     (0x0E)
+#define CONSTRAIN     (0x0F)
+#define RANDOMINT     (0x10)
+#define CMPE          (0x11)
+#define CMPNE         (0x12)
+#define CMPL          (0x13)
+#define CMPLE         (0x14)
+#define CMPG          (0x15)
+#define CMPGE         (0x16)
+#define TRUE          (0x17)
+#define FALSE         (0x18)
+#define NEGATE        (0x19)
+#define NULL_         (0x1A)
+#define GET           (0x1B)
+#define SET           (0x1C)
+#define JUMPZ         (0x1D)
+#define JUMP          (0x1E)
+#define PRINT         (0x1F)
+#define BOOLEAN       (0x20)
+#define STOP          (0x21)
+
+#define STACK_SIZE    (512)
+#define MEMORY_SIZE   (1024)
+#define CODE_SIZE     (1024)
+
+// need a STOP
+// int16_t for memory and stack for negative values
+// no vector;
+// read in bin file
 
 // using namespace std;
 
 
-int getStack(std::stack <int> s) {
-  if (s.empty()) {
-    std::cout<<"empty stack, pop is not allowed"<<std::endl;
-    return 1;
+uint8_t code[CODE_SIZE];
+size_t ip = 0;
+
+uint16_t stack[STACK_SIZE];
+size_t stack_ptr = 0;
+
+uint16_t memory[MEMORY_SIZE];
+
+void push(uint16_t data) {
+  assert(stack_ptr < STACK_SIZE && "stack overflow");
+  stack[stack_ptr++] = data;
+}
+
+uint16_t pop() {
+  assert(stack_ptr >= 0 && "stack underflow");
+  return stack[stack_ptr--];
+}
+
+// 1 = true, 0 = false
+void run() {
+  uint16_t op1;
+  uint16_t op2;
+  for(;;) {
+    switch(code[ip++]) {
+      case NOP_:
+        break;
+      case NUMBER:
+        {
+          uint16_t value = code[ip++];
+          value |= (code[ip++] << 8);
+          push(value);
+        }
+        break;
+      case ADD:
+        {
+          op1 = pop();
+          op2 = pop();
+          push(op1 + op2);
+        }
+        break;
+      case SUB:
+        op1 = pop();
+        op2 = pop();
+        push(op2 - op1);
+        break;
+      case MUL:
+        op1 = pop();
+        op2 = pop();
+        push(op1 * op2);
+        break;
+      case DIV:
+        op1 = pop();
+        assert(op1 != 0 && "Invalid zero denominator");
+        op2 = pop();
+        push(op2 / op1);
+        break;
+      case POW:
+        op1 = pop();
+        op2 = pop();
+        push(pow(op2, op1));
+        break;
+      case ABS:
+        op1 = pop();
+        if (op1 < 0) {
+          op1 = op1*-1;
+        }
+        push(op1);
+        break;
+      case NEG:
+        op1 = pop();
+        push(op1*-1);
+        break;
+      case ISEVEN:
+        op1 = pop();
+        if (op1%2 == 0) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case ISODD:
+        op1 = pop();
+        if (op1%2 == 1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case ISPOSITIVE:
+        op1 = pop();
+        if (op1 >= 0) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case ISNEGATIVE:
+        op1 = pop();
+        if (op1 < 0) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case ISDIVISBLEBY:
+        op1 = pop();
+        op2 = pop();
+        if (op2 % op1 == 0) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case REMAINDER:
+        op1 = pop();
+        op2 = pop();
+        push(op2 % op1);
+        break;
+      case CONSTRAIN:
+        op1 = pop();
+        op2 = pop();
+        if (op1 < op2) {
+          op1 = op2;
+        }
+
+        op2 = pop();
+        if (op1 > op2) {
+          op1 = op2;
+        }
+        push(op1);
+        break;
+      case RANDOMINT:
+       std::cout<<"cannot test random integer, default number is 2"<<"\n";
+       push(2);
+       break;
+      case CMPE:
+        op1 = pop();
+        op2 = pop();
+        if (op2 == op1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case CMPNE:
+        op1 = pop();
+        op2 = pop();
+        if (op2 != op1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case CMPL:
+        op1 = pop();
+        op2 = pop();
+        if (op2 < op1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case CMPLE:
+        op1 = pop();
+        op2 = pop();
+        if (op2 <= op1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case CMPG:
+        op1 = pop();
+        op2 = pop();
+        if (op2 > op1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case CMPGE:
+        op1 = pop();
+        op2 = pop();
+        if (op2 >= op1) {
+          push(0);
+        } else {
+          push(1);
+        }
+        break;
+      case NEGATE:
+        op1 = pop();
+        if (op1 == 0) {
+          push(1);
+        } else if (op1 == 1) {
+          push(0);
+        } else {
+          assert((op1 == 0 || op1 == 1) && "Invalid stack element for negate");
+        }
+        break;
+      case NULL_:
+        push(-1); // need to check this ???
+      case GET:
+        { uint16_t addr=pop();
+          push(memory[addr]);
+        }
+        break;
+      case SET:
+        { uint16_t addr = code[ip++];
+          addr |= (code[ip++]<<8);
+          uint16_t value = pop();
+          memory[addr] = value;
+        }
+        break;
+      case JUMPZ:
+        { uint16_t value=pop();
+          uint16_t addr = code[ip++];
+          addr |= (code[ip++]<<8);
+          if (value != 0) {
+             ip = addr;
+          }
+        }
+        break;
+      case JUMP:
+        { uint16_t addr = code[ip++];
+          addr |= (code[ip++]<<8);
+          ip = addr;
+        }
+        break;
+      case PRINT:
+        {
+          uint16_t data = pop();
+          std::cout << data << std::endl;
+        }
+        break;
+      case BOOLEAN:
+        ++ip;
+        if (code[ip] == TRUE) {
+          push(0);
+        } else if (code[ip] == FALSE) {
+          push(1);
+        } else {
+          std::cerr<<"Invalid command: "<<code[ip]<<std::endl;
+          return;
+        }
+        break;
+      case STOP:
+        return;
+      default:
+        std::cerr<<"Invalid command: "<<code[ip]<<std::endl;
+        return;
+      }
   }
-  int get = s.top();
-  s.pop();
-  return get;
 }
 
 int main (int argc, char *argv[]) {
+
   if (argc != 2) {
     std::cout<<"No specified file to open ";
     std::cout<<"or too many arguments"<<std::endl;
     return 1;
   }
 
-  // open a file in read mode.
-  std::ifstream file;
-  file.open(argv[1], std::ifstream::binary|std::ifstream::in);
-
-  if (!file.is_open()) {
-    std::cout<<"Cannot open the file"<<std::endl;
-    return 1;
+  if(FILE *file = fopen(argv[1],"rb")) {
+     fread(code, CODE_SIZE, 1, file);
+     // run();
+  } else {
+     std::cerr<<"Cannot open file: "<<argv[1]<<std::endl;
   }
-
-  std::string command;
-  std::vector<int> program;
-  // std::map<uni8_t, int> jumpPos;
-  int code;
-  unsigned int counter = 0;
-  while (file >> command) {
-    code = std::stoi(command,nullptr,16);
-    program.push_back(code);
-    // std::cout<<"code type: "<<typeid(code).name()<<" program type: "<<typeid(program[counter]).name();
-    std::cout<<" program: "<<program[counter]<<std::endl;
-    // if ((program[counter].compare(JUMP) == 0) || (program[counter].compare(JUMPZ) == 0)) {
-    //   ++counter;
-    //   file>>command;
-    //   program.push_back(command);
-    // } else if ((command.at(0) == 'L' && command.at(1) == '0' && command.at(2) == '_') ||
-    //            (command.at(0) == 'L' && command.at(1) == '1' && command.at(2) == '_')) {
-    //   jumpPos[command] = counter;
-    //   program[counter] = "nop";
-    // }
-    ++counter;
-  }
-
-  // counter = 0;
-  // while(counter != program.size()) {
-  //   command = program[counter];
-  //   if ((command.at(0) == 'L' && command.at(1) == '0' && command.at(2) == '_') ||
-  //       (command.at(0) == 'L' && command.at(1) == '1' && command.at(2) == '_')) {
-  //     program[counter] = std::to_string(jumpPos[command]);
-  //   }
-  //   ++counter;
-  // }
-
-  counter = -1;
-  std::map<std::string, int> intVar;
-  std::stack <int> s;
-  int a;
-  int b;
-  while(counter != program.size()) {
-    ++counter;
-    switch (program[counter]) {
-      case NUMBER:
-        ++counter;
-        s.push(program[counter]);
-        break;
-      case ADD:
-        a = getStack(s);
-        b = getStack(s);
-        s.push(a + b);
-        break;
-      case SUB:
-        a = getStack(s);
-        b = getStack(s);
-        s.push(b - a);
-        break;
-      case MUL:
-        a = getStack(s);
-        b = getStack(s);
-        s.push(a * b);
-        break;
-      case DIV:
-        a = getStack(s);
-        if (a <= 0) {
-          std::cout<<"Invalid denominator: "<<a<<std::endl;
-          return 1;
-        }
-        b = getStack(s);
-        s.push(b / a);
-        break;
-      case POW:
-        a = getStack(s);
-        b = getStack(s);
-        s.push(pow(b, a));
-        break;
-      case ABS:
-        a = getStack(s);
-        if (a < 0) {
-          a = a*-1;
-        }
-        s.push(a);
-        break;
-      case NEG:
-        a = getStack(s);
-        a = a*-1;
-        s.push(a);
-        break;
-      case ISEVEN:
-        a = getStack(s);
-        s.push(a%2);
-        break;
-      case ISODD:
-        a = getStack(s);
-        if (a%2 == 0) {
-          s.push(1);
-        } else {
-          s.push(0);
-        }
-        break;
-      case ISPOSITIVE:
-        a = getStack(s);
-        if (a >= 0) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case ISNEGATIVE:
-        a = getStack(s);
-        if (a < 0) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case ISDIVISBLEBY:
-        a = getStack(s);
-        b = getStack(s);
-        if (b % a == 0) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case REMAINDER:
-        a = getStack(s);
-        b = getStack(s);
-        s.push(b % a);
-        break;
-      case CONSTRAIN:
-        a = getStack(s);
-        b = getStack(s);
-        if (a < b) {
-          a = b;
-        }
-
-        b = getStack(s);
-        if (a > b) {
-          a = b;
-        }
-        s.push(a);
-        break;
-      case RANDOMINT:
-       std::cout<<"cannot test random, default number is 2 "<<program[counter]<<"\n";
-       s.push(2);
-       break;
-      case CMPE:
-        a = getStack(s);
-        b = getStack(s);
-        if (b == a) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case CMPNE:
-        a = getStack(s);
-        b = getStack(s);
-        if (b != a) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case CMPL:
-        a = getStack(s);
-        b = getStack(s);
-        if (b < a) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case CMPLE:
-        a = getStack(s);
-        b = getStack(s);
-        if (b <= a) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case CMPG:
-        a = getStack(s);
-        b = getStack(s);
-        if (b > a) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case CMPGE:
-        a = getStack(s);
-        b = getStack(s);
-        if (b >= a) {
-          s.push(0);
-        } else {
-          s.push(1);
-        }
-        break;
-      case NEGATE:
-        a = getStack(s);
-        if (a == 0) {
-          s.push(1);
-        } else if (a == 1) {
-          s.push(0);
-        } else {
-          std::cout<<"Invalid stack element for negate: "<<a<<std::endl;
-          return 1;
-        }
-        break;
-      case NULL_:
-        s.push(-1); // need to check this ???
-      case GET:
-        // ++counter;
-        // std::string varName = program[counter];
-        // result = intVar[varName];
-        // s.push(result);
-        break;
-      case SET:
-        // a = getStack(s);
-        // ++counter;
-        // std::string varName = program[counter];
-        // intVar[varName] = a;
-        break;
-      case JUMPZ:
-        // ++counter;
-        // int condition = getStack(s);
-        // if (condition == 1) {
-        //   if (program[counter] >= program[counter].size() || program[counter] < 0) {
-        //     std::cout<<"Invalid jump number\n"<<std::endl;
-        //     return 1;
-        //   }
-        // }
-        break;
-      case JUMP:
-        // ++counter;
-        // if (program[counter] >= program[counter].size() || program[counter] < 0) {
-        //   std::cout<<"Invalid jump number\n"<<std::endl;
-        //   return 1;
-        // }
-        break;
-      case PRINT:
-        a = getStack(s);
-        std::cout<<a<<"\n";
-        break;
-      case BOOLEAN:
-        ++counter;
-        if (program[counter] == TRUE) {
-          s.push(0);
-        } else if (program[counter] == FALSE) {
-          s.push(1);
-        } else {
-          std::cout<<"Invalid command: "<<program[counter]<<std::endl;
-          return -1;
-        }
-        break;
-      case NOP_:
-        break;
-      default:
-        std::cout<<"Invalid command: "<<program[counter]<<std::endl;
-        return 1;
-    }
-  }
-
-  if (s.empty()) {
-    std::cout<<"empty stack, pop is not allowed"<<std::endl;
-    return 0;
-  }
-  std::cout<<s.top()<<std::endl;
-  s.pop();
-  file.close();
-  return 0;
 }
