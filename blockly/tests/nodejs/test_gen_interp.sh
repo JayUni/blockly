19bfd1fc2035ab5be0ccdf1b783a0aadd2ea5e67
JayUni <jzhe3972@uni.sydney.edu.au>
2019-05-20 01:44:16 +1000

    debugging

--- blockly/tests/nodejs/test_gen_interp.sh
+++ blockly/tests/nodejs/test_gen_interp.sh
@@ -5,7 +5,7 @@
 g++ ../../SkoolBot/interpreter_binary.cpp -o ../../SkoolBot/interpreter
 # for gen in bin_generator_outputs/*.bin
 # do
-    gen="bin_generator_outputs/for_if_constraint_abs_even_neg.bin"
+    gen="bin_generator_outputs/simple_if_else.bin"
     interpreter="interpreter_final_outputs/`basename $gen .bin`.txt"
 
    if [ -e $interpreter ]
