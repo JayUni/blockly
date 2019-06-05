1170f95b74f4bd5fe3e16dd2556d94844f45c3e2
ylin9811 <ylin9811@uni.sydney.edu.au>
2019-06-05 20:28:05 +1000

    fixed the problem of for loop

--- blockly/Skoolbot/blockly_chrome_app/code/msg/en.js
+++ blockly/Skoolbot/blockly_chrome_app/code/msg/en.js
@@ -14,7 +14,7 @@ var MSG = {
   catColour: "Colour",
   catVariables: "Variables",
   catFunctions: "Functions",
-  catInputoutput: "Inputoutput",
+  catInputoutput: "I/O",
   listVariable: "list",
   textVariable: "text",
   httpRequestError: "There was a problem with the request.",
